a4d6fd0d10bfee8ae115fa039a9df494412644eb
mohamed mohamed <ana_mido1998@hotmail.com>
2019-04-05 02:13:28 +0000

    added functions

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
-int main {
+/* Hello World program */
 
-printf("hello world");
+#include<stdio.h>
 
-return 0;
+#include <stdio.h>
+int e(int x);
+int e(char c);
+
+int main()
+{
+   int k;
+   char m,c;
+
+    return 0;
+} 
+
+int e(int x){
+    char m;
+    x = 26;
+    e[x] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    e[x] = (m + k)*(% 26);
+    return e;
+    
 }
+
+int e(int c){
+    char c;
+    c = 26
+    e[c] = {a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z};
+    e[c] = (c - k)*(% 26);
+    return e;   
+} 
+    
