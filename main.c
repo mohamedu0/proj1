6012ea5a5b780429fe4a4b11636206178b82b638
mohamed mohamed <ana_mido1998@hotmail.com>
2019-04-21 07:57:52 +0000

    encripting 1

--- main.c
+++ main.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
-char e (char x);
 
- 
-char d (char c);
-
- 
+ char enrotationcipher(char msg[1024], int key);
  
 int 
 main () 
 {
   
  
-int k;
+char msg[1024],code;
+int key , i;
   
-int n = 0;
-  
-char m, c, x, ch;
-  ch = m;
- printf
-    ("if you want to encript please enter 1\n if you want to decript enter 2\n ");
-  
-scanf ("%d", &n);
-  
-printf ("please enter text:\n");
-  
- 
-scanf ("%c", &ch);
+ printf("what is the word\n");
+    scanf("%s", &msg);
   
 
  
@@ -33,32 +19,15 @@ scanf ("%c", &ch);
 printf ("please enter key\n");
   
  
-scanf ("%d", &k);
+scanf ("%d", &key);
   
-if (n = 1)
-    {
+
+    code = enrotationcipher(msg, key);
+    code = msg;
       
-m = e (x);
-      
- 
-printf ("%c", m);
-    }
-  
-  else if (n = 2)
-    {
-      
-m = d (x);
-      
-printf ("%c", m);
+  printf("new text is :%s\n",msg);
     
-}
-  
-  else
-    {
-      
-printf ("enter a number");
-    
-}
+
   
  
 return 0;
@@ -66,63 +35,14 @@ return 0;
  
 }
 
-
- 
- 
- 
-char 
-e (char x) 
-{
-  
- 
- 
-char m;
-  
- 
-int k;
-  
- 
- 
- 
-char e[26] = "this is a sting";
-  
- 
-e[m] = m + k;
-  
- 
-return m;
-
- 
- 
-}
-
-
- 
- 
- 
-char
-d (char x) 
-{
-  
- 
-int k;
-  
- 
-char c;
-  
- 
- 
-char d[26] = "this is a sting";
-  
- 
-d[c] = (c - k);
-  
- 
-return c;
-
- 
-}
-
-
- 
+char enrotationcipher(char msg[1024], int key){
+  char code;
+  int i;
+  for (i = 0; msg[i] != '\0'; ++i){
+   code = msg[i];
+    code = code + key;
+  msg[i] = code;
+        return msg ;
+    } 
+ }
  
