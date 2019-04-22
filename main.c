2a2f1ff14bde14196001ac3b351a34655c48481f
mohamed mohamed <ana_mido1998@hotmail.com>
2019-04-22 07:19:28 +0000

    new function

--- main.c
+++ main.c
@@ -1,38 +1,44 @@
 #include <stdio.h>
 
  char enrotationcipher(char msg[1024], int key);
+ char decribtioncipher(char msg[1024], int key);
  
 int 
 main () 
 {
+  
  FILE *input;
  FILE *output;
- 
+ FILE *filekey;
 char msg[1024],code;
-int key , i;
-  input = fopen("input.txt","r");
+int key;
+int i,n;
+  
+input = fopen("input.txt","r");
+filekey = fopen("key.txt", "r");
   output = fopen("output.txt", "w");
-  fscanf(input,"%s",&msg);
- //printf("what is the word\n");
-  //  scanf("%s", &msg);
-  
-
+printf("1) encript\n 2)decript\n");
+scanf("%d",&n);
+ while (feof(input)== 0){
  
- 
-//printf ("please enter key\n");
-  
- 
-//scanf ("%d", &key);
-  
-
+ if(n = 1){
+    fscanf(input,"%s\n", &msg);
+    fscanf(filekey,"%d", &key);
     code = enrotationcipher(msg, key);
     code = msg;
-      
- fprintf(output,"%s",msg);
-    
+  }
+  else if (n = 2){
+      fscanf(input,"%s\n", &msg);
+    fscanf(filekey,"%d", &key);
+    code = decribtioncipher(msg,key);
+    code = msg;
+  }
 
   
- 
+    
+}
+  fprintf(output,"%s\n ",msg);
+ fprintf(output,"the key: %d",key);
 return 0;
 
  
@@ -41,11 +47,26 @@ return 0;
 char enrotationcipher(char msg[1024], int key){
   char code;
   int i;
+  msg[1024] = "this is a string";
   for (i = 0; msg[i] != '\0'; ++i){
    code = msg[i];
     code = code + key;
   msg[i] = code;
-        return msg ;
+       
     } 
+  return code;
+ }
+ 
+char decribtioncipher(char msg[1024], int key){
+  char code;
+  int i;
+  msg[1024] = "this is a string";
+  for (i = 0; msg[i] != '\0'; ++i){
+   code = msg[i];
+    code = code - key;
+  msg[i] = code;
+       
+    } 
+  return code;
  }
  
