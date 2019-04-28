735d2cc972229ba362a58f6ce21bf3b741e8f68e
mohamed mohamed <ana_mido1998@hotmail.com>
2019-04-28 13:48:14 +0000

    done

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 /* this coude id ude to encrypt and decrypt useing the Rotation Cipher and the Substitution Cipher methods.
 - when trying to  encrypt or decrypt by the rotation cipher please use the key.txt file to put the key 
 - please use the input.txt file to put the words you would like to  encrypt or decrypt*/
-  char *enrotationrotaion(char *msg[1024], int key); // rotation cipher encription function header
- char *decribtionrotaion(char *msg[1024], int key);  // rotation cipher dencription function header
- char *encrybtsub(char *msg,char *emsg);     // Substitution Cipher encription function header
-char  *decrybtsub(char *msg,char *dmsg);     // Substitution Cipher dencription function header
+     char *enrotationrotaion(char *msg[1024], int key); // rotation cipher encription function header
+    char *decribtionrotaion(char *msg[1024], int key);  // rotation cipher dencription function header
+    char *encrybtsub(char *msg,char *emsg);     // Substitution Cipher encription function header
+    char  *decrybtsub(char *msg,char *dmsg);     // Substitution Cipher dencription function header
 int 
 main () 
 {
@@ -19,7 +19,7 @@ int i,n,count, spaces = 0;
   
 input = fopen("input.txt","r");         //opening file input.txt to read from when using input
 filekey = fopen("key.txt", "r");        //opening file key.txt to read from when using filekey
-  output = fopen("output.txt", "w");    //opening file output.txt to read from when using output
+output = fopen("output.txt", "w");    //opening file output.txt to read from when using output
   
   
 printf("please enter a number according to the following order \n1) encrypt using rotation cipher\n 2)decrypt using rotation cipher\n 3)encrypt usong substitution cipher\n 4)decrypt using sustitution cipher\n");
@@ -28,28 +28,28 @@ scanf("%d",&n); // takes the number used by the user to use the function using t
 /*the following function are done till there is noting else to scan from input.txt file*/
  while (feof(input)== 0){    // this is a loob to make sure that input.txt is scaned till there is nothing else is needed to be scand
  
- if(n == 1) { //if user enters 1
-    fscanf(input,"%s\n", &msg);     //scan the text that was writen in input.txt and places it in the value msg
-    fscanf(filekey,"%d", &key);     //scan the number that was writen in key.txt  and places it in the value key
-    code = enrotationcipher(msg, key); //takes the value from the function enrotationcipher and places it as the value code
-    code = msg;                         
+     if(n == 1) { //if user enters 1
+             fscanf(input,"%s\n", &msg);     //scan the text that was writen in input.txt and places it in the value msg
+             fscanf(filekey,"%d", &key);     //scan the number that was writen in key.txt  and places it in the value key
+             code = enrotationcipher(msg, key); //takes the value from the function enrotationcipher and places it as the value code
+             code = msg;                         
   }
-  else if (n == 2){  //if user enters 2 
-      fscanf(input,"%s\n", &msg);    //scan the text that was writen in input.txt and places it in the value msg
-    fscanf(filekey,"%d", &key);         //scan the number that was writen in key.txt  and places it in the value key
-    code = decribtioncipher(msg,key);   //takes the value from the function decribtioncipher and places it as the value code 
-    code = msg;
+        else if (n == 2){  //if user enters 2 
+             fscanf(input,"%s\n", &msg);    //scan the text that was writen in input.txt and places it in the value msg
+             fscanf(filekey,"%d", &key);         //scan the number that was writen in key.txt  and places it in the value key
+             code = decribtioncipher(msg,key);   //takes the value from the function decribtioncipher and places it as the value code 
+             code = msg;
     
   }
- else if(n==3){ //if user enters 3
- fscanf(input,"%s\n", &msg);    //scan the text that was writen in input.txt and places it in the value msg
- code =  char encrybtsub( msg, emsg);  //takes the value from the function enrotationsubst and places it as the value code
-    code = msg;
+        else if(n==3){ //if user enters 3
+                fscanf(input,"%s\n", &msg);    //scan the text that was writen in input.txt and places it in the value msg
+            code = encrybtsub( msg, emsg);  //takes the value from the function enrotationsubst and places it as the value code
+             code = msg;
 }
-else if(n==4){  //if user enters 4
-    scanf(input,"%s\n", &msg);  //scan the text that was writen in input.txt and places it in the value msg
- code = decrybtsub( msg,dmsg);     //takes the value from the function decribtionsubst and places it as the value code
-    code = msg;
+        else if(n==4){  //if user enters 4
+                scanf(input,"%s\n", &msg);  //scan the text that was writen in input.txt and places it in the value msg
+                 code = decrybtsub( msg,dmsg);     //takes the value from the function decribtionsubst and places it as the value code
+                     code = msg;
 }
 }
 
@@ -57,10 +57,13 @@ else if(n==4){  //if user enters 4
     
 
   fprintf(output,"%s\n ",msg); //prints the enctypted or decrypted text on the file output.txt
-return 0;
 
+return 0;
  
 }
+
+
+
 /*this function enctypts the text given from the user using the rotation cipher  */
 char enrotationcipher(char* msg[1024], int key){
   char code; 
@@ -91,9 +94,9 @@ char enrotationcipher(char* msg[1024], int key){
 		}
 }
 
-    
   return code;
  }
+ 
  
  
  /*this function denctypts the text given from the user using the rotation cipher  */
@@ -124,12 +127,11 @@ char decribtioncipher(char* msg[1024], int key){
 			msg[i] = code;
 		}
 }
-
-
-    
+ 
   return code;
  }
  
+
 
 /*this function is used for encrypting a text from the user usong the substitution cipher */
 char *encrybtsub(char *msg,char *emsg){
@@ -154,6 +156,7 @@ char *encrybtsub(char *msg,char *emsg){
            
          return code;
    }
+
 
     
 /*this function is used for dencrypting a text from the user usong the substitution cipher */
